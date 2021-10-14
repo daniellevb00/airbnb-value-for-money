488a426b1a164845c9ea3561fffbd28d1574f5cc
Lesley Haerkens <lesleyhaerkens@air-van-lesley.home>
2021-10-14 16:45:20 +0200

    re-organized repository: updated makefile, created 3 seperate files from clean.R to enhance clarity

--- src/data-preparation/download.R
+++ src/data-preparation/download.R
@@ -1,36 +1,19 @@
-### DPREP PROJECT - TEAM 3 - InsideAirbnb - DOWNLOAD FILE ###
-## Downloads the raw listings data from InsideAirbnb 
-## This file should store the data inside the data folder
-# Run this script from the terminal using: R --vanilla < download.R
+######################
+### DOWNLOAD DATA ####
+######################
 
-#To download data programmatically from the web; downloads a file from a URL and stores it on your local machine. 
-download_data <- function(url, filename, filepath){ #create a function to download the data
-  dir.create(filepath) #create a directory
-  download.file(url=url, destfile = paste0(filepath,filename)) #download the file
+# Creating the data folder
+dir.create('../../data')
+
+# creating a list with the download link and filenames of the raw data 
+files = list(c(url='http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-09-07/data/listings.csv.gz',
+               fn='listings-amsterdam.csv.gz'),
+             c(url='http://data.insideairbnb.com/belgium/vlg/antwerp/2021-09-29/data/listings.csv.gz',
+               fn='listings-antwerp.csv.gz'))
+
+# looping over the list to download and save the file in the data folder
+for (item in files) {
+  
+  download.file(item['url'], paste0('../../data/', item['fn']))
 }
-
-#Downloading the raw listings data for Amsterdam
-#REMEMBER: the filepath is dependent on the location from where your script is called, so the use of absolute directory names should be avoided so that the code remains portable to other computers and work environments!
-download_data(url = 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-09-07/data/listings.csv.gz', filename='Listings.csv',filepath='data/')
-
-#Opening the raw listings data for Amsterdam
-airbnb<-read.table('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-09-07/data/listings.csv.gz', sep =',', header=TRUE)
-
-
-
-
-
-#(What the code looked like before:)
-download.file('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-08-06/data/listings.csv.gz', 'Listings.csv')
-
-#...add the download links for the other cities we want to analyse... 
-
-
-
-
-# I'm not sure whether we need to include this, maybe this step goes with the clean.R file. 
-library(readr)
-df<-read.csv('Listings.csv')
-head(df)
-View(df)
 
